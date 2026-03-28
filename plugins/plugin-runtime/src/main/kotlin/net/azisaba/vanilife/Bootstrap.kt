905cc2c980ab9903c92df167bcc984b71820c00d
tksimeji <tksimeji@outlook.com>
2026-03-28 10:02:30 +0900

    chore: add plugin-runtime classes

---
+++ plugins/plugin-runtime/src/main/kotlin/net/azisaba/vanilife/Bootstrap.kt
@@ -0,0 +1,9 @@
+package net.azisaba.vanilife.runtime
+
+import io.papermc.paper.plugin.bootstrap.BootstrapContext
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap
+
+internal class Bootstrap : PluginBootstrap {
+    override fun bootstrap(context: BootstrapContext) {
+    }
+}
---
+++ plugins/plugin-runtime/src/main/kotlin/net/azisaba/vanilife/Main.kt
@@ -0,0 +1,5 @@
+package net.azisaba.vanilife.runtime
+
+import org.bukkit.plugin.java.JavaPlugin
+
+internal class Main : JavaPlugin()
